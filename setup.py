6f2ec4565c720019f9bdf762cf855977251e0b26
Fabrice POIRIER <fabrice.poirier@ensta-bretagne.org>
2019-07-09 12:06:06 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="item_tracking",
-    version="1.0.4",
+    version="1.0.5",
     author="Fabrice POIRIER",
     author_email="fabrice.poirier@ensta-bretagne.org",
     description="A tracking algorithm for general purpose",
