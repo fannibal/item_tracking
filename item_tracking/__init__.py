b4a4b2a1c3f2912b7496e407670da54016a6d413
fannibal <fabrice.poirier@ensta-bretagne.org>
2020-10-12 11:39:08 +0200

    Update __init__.py
    update __init__.py for python 3

--- item_tracking/__init__.py
+++ item_tracking/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from tracker import Tracker
-from item import Item, Component
+from .tracker import Tracker
+from .item import Item, Component
 
 name = "item_tracking"
